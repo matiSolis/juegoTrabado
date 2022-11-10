0b3f95c51718399f25d2028282a81b0ad2e66cba
matiSolis <solis.matias.n@gmail.com>
2022-11-10 19:26:59 -0300

    codigo terminado

--- js/main.js
+++ js/main.js
@@ -1,20 +1,4 @@
-// CONSTRUCTORES DEL JUEGO
-class Personaje {
-    constructor(tipo, vida, ataques) {
-        this.tipo = tipo;
-        this.vida = vida;
-        this.ataques = ataques;
-    }
-}
-
-class AtaquePj {
-    constructor(id, nombre, daño) {
-        this.id = id;
-        this.nombre = nombre;
-        this.daño = daño;
-    }
-}
-
+// CONSTRUCTORES DEL JUEGO 
 class Monstruos {
     constructor(tipo, vida, daño) {
         this.tipo = tipo;
@@ -24,43 +8,13 @@ class Monstruos {
 }
 
 //=====================================================================================================
-//MAGO
-const ataqueMago1 = new AtaquePj(1, "Bola de fuego", 20);
-const ataqueMago2 = new AtaquePj(2, "Rayo", 25);
-const ataqueMago3 = new AtaquePj(3, "Rayo de escarcha", 20);
-
-const ataqueMago = [ataqueMago1, ataqueMago2, ataqueMago3];
-
-const mago = new Personaje("Mago", 100, ataqueMago);
-
-//=====================================================================================================
-//GUERRERO
-const ataqueGuerrero1 = new AtaquePj(1, "Filo resonante", 20);
-const ataqueGuerrero2 = new AtaquePj(2, "Destierro", 25);
-const ataqueGuerrero3 = new AtaquePj(3, "Estallido de filos", 20);
-
-const ataqueGuerrero = [ataqueGuerrero1, ataqueGuerrero2, ataqueGuerrero3];
-
-const guerrero = new Personaje("Guerrero", 100, ataqueGuerrero);
-
-//=====================================================================================================
-//ARQUERA
-const ataqueArquera1 = new AtaquePj(1, "Flecha de cristal", 20);
-const ataqueArquera2 = new AtaquePj(2, "Disparo de halcon", 25);
-const ataqueArquera3 = new AtaquePj(3, "Flecha de fuego", 20);
-
-const ataqueArquera = [ataqueArquera1, ataqueArquera2, ataqueArquera3];
-
-const arquera = new Personaje("Arquera", 100, ataqueArquera);
-
-//=====================================================================================================
 //MONSTRUO
 
 const espiritu = new Monstruos("Espiritu", 40, 5);
 const huargo = new Monstruos("Huargo", 60, 15);
 const orco = new Monstruos("Orco", 70, 20);
-const troll = new Monstruos("Troll", 90, 25);
-const balrog = new Monstruos("Balrog", 120, 35);
+const troll = new Monstruos("Troll", 80, 25);
+const balrog = new Monstruos("Balrog", 90, 30);
 
 
 const monstruosArray = [espiritu, huargo, orco, troll, balrog];
@@ -72,6 +26,9 @@ let indicador;
 let critico;
 let pjJSON;
 let seleccionPersonaje;
+let ataque1;
+let ataque2;
+let ataque3;
 
 const vidaPj = document.getElementById('vidaPj');
 const vidaMonstruo = document.getElementById('vidaMonstruo');
@@ -86,17 +43,17 @@ function esconderBotonPersonajes() {
 function esconderBotonSelecEnemigo() {
     selecEnemigo();
     vidaMonstruo.innerText = `${enemigo.tipo} vida ${enemigo.vida}`;
+    vidaPj.innerText = `${seleccionPersonaje.tipo} vida ${seleccionPersonaje.vida}`;
     document.getElementById('btnSeleccionarEnemigo').style.display = 'none';
     document.getElementById('btnComenzar').style.display = 'block';
     let divCajaTexto = document.getElementById('divTexto');
-    divCajaTexto.innerText = `La montaña maldita.
+    divCajaTexto.innerText = `La montaña maldita
 Muchos años atrás en Carnicabunstein ,un pueblo cercano a la gran montaña, un grupo de magos forjo una daga mágica. Esta daga le otorga a su portador la habilidad de controlar a los dragones. El pueblo entro en guerra tras guerra ya que todos querían poseer el poder otorgado por el filoso tesoro. Tal fue el daño causado por la daga que sus creadores tomaron la drástica decisión de perderla en las catacumbas de la montaña y sellar la entrada con una maldición.
 Cientos de años pasaron y una gran amenaza se fue haciendo más fuerte en el continente. Los ciudadanos de Carnicabunstein necesitan nuevamente la ayuda de los dragones, pero para poder controlarlos la daga deberán recuperar.Estas listo para encontrar la daga y enfrentarte a las maldiciones de los antiguos magos?`;
 }
 
 function esconderBotonComenzar() {
     document.getElementById('btnComenzar').style.display = 'none';
-    vidaPj.innerText = `${seleccionPersonaje.tipo} vida ${seleccionPersonaje.vida}`;
     document.getElementById('divTexto').style.display = 'none';
     document.getElementById('btnAtaque1').style.display = 'block';
     document.getElementById('btnAtaque2').style.display = 'block';
@@ -120,62 +77,112 @@ function esconderBotonTurnoEnemigo() {
 }
 
 function selecEnemigo() {
-    enemigo = monstruosArray[0];
-    return enemigo;
+    if (monstruosArray.length > 0) {
+        enemigo = monstruosArray[0];
+        return enemigo;
+    } else {
+        Swal.fire({
+            title: 'LA DAGA ES NUESTRA!',
+            text: 'Derrotas al ultimo enemigo y la daga se revela ante nosotros. Con ella lograremos controlar los dragones!. La victoria de nuestro pueblo es inminente. FELICITACIONES, ERES UN HEROE!',
+            imageUrl: './assets/dagaDragon.jpg',
+            imageWidth: 200,
+            imageHeight: 200,
+            background: '#282626',
+            imageAlt: 'Victoria',
+        })
+        document.getElementById('btnComenzar').style.display = 'none';
+    }
 }
 
-function selecMago() {
-    seleccionPersonaje = mago;
-    let pjJSON = JSON.stringify(mago);
-    localStorage.setItem("Personaje seleccionado", pjJSON);
-    esconderBotonPersonajes();
-    return seleccionPersonaje;
+const selecMago = () => {
+    fetch("data/data.json")
+        .then((respuesta) => {
+            return respuesta.json();
+        })
+        .then((mago) => {
+            seleccionPersonaje = mago[0];
+            let pjJSON = JSON.stringify(mago[0]);
+            localStorage.setItem("Personaje seleccionado", pjJSON);
+            esconderBotonPersonajes();
+            return seleccionPersonaje;
+        })
+        .catch((error) => {
+            console.log("ERROR" + error);
+        })
 }
 
-function selecGuerrero() {
-    seleccionPersonaje = guerrero;
-    let pjJSON = JSON.stringify(guerrero);
-    localStorage.setItem("Personaje seleccionado", pjJSON);
-    esconderBotonPersonajes();
-    return seleccionPersonaje;
+const selecGuerrero = () => {
+    fetch("data/data.json")
+        .then((respuesta) => {
+            return respuesta.json();
+        })
+        .then((guerrero) => {
+            seleccionPersonaje = guerrero[1];
+            let pjJSON = JSON.stringify(guerrero[1]);
+            localStorage.setItem("Personaje seleccionado", pjJSON);
+            esconderBotonPersonajes();
+            return seleccionPersonaje;
+        })
+        .catch((error) => {
+            console.log("ERROR" + error);
+        })
 }
 
-function selecArquera() {
-    seleccionPersonaje = arquera;
-    let pjJSON = JSON.stringify(arquera);
-    localStorage.setItem("Personaje seleccionado", pjJSON);
-    esconderBotonPersonajes();
-    return seleccionPersonaje;
+const selecArquera = () => {
+    fetch("data/data.json")
+        .then((respuesta) => {
+            return respuesta.json();
+        })
+        .then((arquera) => {
+            seleccionPersonaje = arquera[2];
+            let pjJSON = JSON.stringify(arquera[2]);
+            localStorage.setItem("Personaje seleccionado", pjJSON);
+            esconderBotonPersonajes();
+            return seleccionPersonaje;
+        })
+        .catch((error) => {
+            console.log("ERROR" + error);
+        })
 }
 
 function enemigoTurno() {
-    if (monstruosArray.length > 0) {
-        if (enemigo.vida <= 0) {
-            seleccionPersonaje.vida += 25;
-            monstruosArray.shift();
-            esconderBotonAtaques();
-            document.getElementById('btnTurnoEnemigo').style.display = 'none';
-            document.getElementById('btnSeleccionarEnemigo').style.display = 'block';
-        } else if (enemigo.vida > 0 && enemigo.vida < 16) {
-            enemigo.vida += 15;
+    if (enemigo.vida <= 0) {
+        Swal.fire({
+            title: 'El enemigo muere',
+            text: 'Derrotas a tu enemigo y sigues bajando por la montaña. Descanza, nuevos peligros nos esperan.',
+            imageUrl: './assets/muerto.jpg',
+            imageWidth: 200,
+            imageHeight: 200,
+            background: '#282626',
+            imageAlt: 'Victoria parcial',
+        })
+        seleccionPersonaje.vida += 25;
+        monstruosArray.shift();
+        esconderBotonAtaques();
+        document.getElementById('btnTurnoEnemigo').style.display = 'none';
+        document.getElementById('btnSeleccionarEnemigo').style.display = 'block';
+    } else if (enemigo.vida > 0 && enemigo.vida < 16) {
+        enemigo.vida += 15;
+        vidaMonstruo.innerText = `${enemigo.tipo} vida ${enemigo.vida}`;
+        vidaPj.innerText = `${seleccionPersonaje.tipo} vida ${seleccionPersonaje.vida}`;
+        esconderBotonTurnoEnemigo();
+    } else {
+        let indicador = Math.floor(Math.random() * 4) + 1;
+        if (indicador == 4) {
+            seleccionPersonaje.vida -= enemigo.daño * 1.5;
             vidaMonstruo.innerText = `${enemigo.tipo} vida ${enemigo.vida}`;
             vidaPj.innerText = `${seleccionPersonaje.tipo} vida ${seleccionPersonaje.vida}`;
-            esconderBotonTurnoEnemigo();
         } else {
             seleccionPersonaje.vida -= enemigo.daño;
             vidaMonstruo.innerText = `${enemigo.tipo} vida ${enemigo.vida}`;
             vidaPj.innerText = `${seleccionPersonaje.tipo} vida ${seleccionPersonaje.vida}`;
             esconderBotonTurnoEnemigo();
         }
-    } else {
-        let divCajaTexto = document.getElementById('divTexto');
-        divCajaTexto.innerText = `Felicitaciones joven ${seleccionPersonaje}!, has logrado recuperar la daga. Con ella y la ayuda de los dragones lograremos traer la paz a nuestro pueblo. FELICITACIONES, HAS GANADO! `;
-        esconderBotonAtaques();
     }
 }
 
 function pocima() {
-    seleccionPersonaje.vida += 25;
+    seleccionPersonaje.vida += 30;
     vidaMonstruo.innerText = `${enemigo.tipo} vida ${enemigo.vida}`;
     vidaPj.innerText = `${seleccionPersonaje.tipo} vida ${seleccionPersonaje.vida}`;
     esconderBotonAtaques();
@@ -184,19 +191,26 @@ function pocima() {
 }
 
 function ataque01() {
+    let ataque1 = seleccionPersonaje.ataque1;
     if (seleccionPersonaje.vida <= 0) {
+        Swal.fire({
+            title: 'GAME OVER',
+            text: `La muerte te ha alcanzado joven ${seleccionPersonaje.tipo}. Tu alma quedara encerrada en la montaña por el resto de la eternidad`,
+            imageUrl: './assets/tumba.jpg',
+            imageWidth: 200,
+            imageHeight: 200,
+            background: '#282626',
+            imageAlt: 'Muerte',
+        })
         esconderBotonAtaques();
-        let divCajaTexto = document.getElementById('divTexto');
-        divCajaTexto.innerText = `Joven ${seleccionPersonaje} tu camino ha llegado al fin, la muerte se ha echo una contigo.`;
     } else {
-        let ataque = seleccionPersonaje.ataques[0];
         let indicador = Math.floor(Math.random() * 4) + 1;
         if (indicador == 4) {
-            enemigo.vida -= ataque.daño * 1.5;
+            enemigo.vida -= ataque1 * 1.5;
             vidaMonstruo.innerText = `${enemigo.tipo} vida ${enemigo.vida}`;
             vidaPj.innerText = `${seleccionPersonaje.tipo} vida ${seleccionPersonaje.vida}`;
         } else {
-            enemigo.vida -= ataque.daño;
+            enemigo.vida -= ataque1;
             vidaMonstruo.innerText = `${enemigo.tipo} vida ${enemigo.vida}`;
             vidaPj.innerText = `${seleccionPersonaje.tipo} vida ${seleccionPersonaje.vida}`;
         }
@@ -206,18 +220,26 @@ function ataque01() {
 }
 
 function ataque02() {
+    let ataque2 = seleccionPersonaje.ataque2;
     if (seleccionPersonaje.vida <= 0) {
-        let divCajaTexto = document.getElementById('divTexto');
-        divCajaTexto.innerText = `Joven ${seleccionPersonaje.tipo} tu camino ha llegado al fin. Vuelve a comenzar con otro Pj para intentar recuperar la daga perdida. `;
+        Swal.fire({
+            title: 'GAME OVER',
+            text: `La muerte te ha alcanzado joven ${seleccionPersonaje.tipo}. Tu alma quedara encerrada en la montaña por el resto de la eternidad`,
+            imageUrl: './assets/tumba.jpg',
+            imageWidth: 200,
+            imageHeight: 200,
+            background: '#282626',
+            imageAlt: 'Muerte',
+        })
+        esconderBotonAtaques();
     } else {
-        let ataque1 = seleccionPersonaje.ataques[1];
         let indicador = Math.floor(Math.random() * 8) + 1;
         if (indicador == 8) {
-            enemigo.vida -= ataque1.daño * 1.5;
+            enemigo.vida -= ataque2 * 1.5;
             vidaMonstruo.innerText = `${enemigo.tipo} vida ${enemigo.vida}`;
             vidaPj.innerText = `${seleccionPersonaje.tipo} vida ${seleccionPersonaje.vida}`;
         } else {
-            enemigo.vida -= ataque1.daño;
+            enemigo.vida -= ataque2;
             vidaMonstruo.innerText = `${enemigo.tipo} vida ${enemigo.vida}`;
             vidaPj.innerText = `${seleccionPersonaje.tipo} vida ${seleccionPersonaje.vida}`;
         }
@@ -227,18 +249,26 @@ function ataque02() {
 }
 
 function ataque03() {
+    let ataque3 = seleccionPersonaje.ataque3;
     if (seleccionPersonaje.vida <= 0) {
-        let divCajaTexto = document.getElementById('divTexto');
-        divCajaTexto.innerText = `Joven ${seleccionPersonaje.tipo} tu camino ha llegado al fin. Vuelve a comenzar con otro Pj para intentar recuperar la daga perdida. `;
+        Swal.fire({
+            title: 'GAME OVER',
+            text: `La muerte te ha alcanzado joven ${seleccionPersonaje.tipo}. Tu alma quedara encerrada en la montaña por el resto de la eternidad`,
+            imageUrl: './assets/tumba.jpg',
+            imageWidth: 200,
+            imageHeight: 200,
+            background: '#282626',
+            imageAlt: 'Muerte',
+        })
+        esconderBotonAtaques();
     } else {
-        let ataque2 = seleccionPersonaje.ataques[2];
         let indicador = Math.floor(Math.random() * 4) + 1;
         if (indicador == 4) {
-            enemigo.vida -= ataque2.daño * 1.5;
+            enemigo.vida -= ataque3 * 1.5;
             vidaMonstruo.innerText = `${enemigo.tipo} vida ${enemigo.vida}`;
             vidaPj.innerText = `${seleccionPersonaje.tipo} vida ${seleccionPersonaje.vida}`;
         } else {
-            enemigo.vida -= ataque2.daño;
+            enemigo.vida -= ataque3;
             vidaMonstruo.innerText = `${enemigo.tipo} vida ${enemigo.vida}`;
             vidaPj.innerText = `${seleccionPersonaje.tipo} vida ${seleccionPersonaje.vida}`;
         }
@@ -246,6 +276,7 @@ function ataque03() {
         document.getElementById('btnTurnoEnemigo').style.display = 'block';
     }
 }
+
 //=====================================================================================================
 //BOTONES
 const btnMago = document.getElementById('mago').addEventListener("click", selecMago);
